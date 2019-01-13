2d9d63e00eef77c77a771d43d8aa5154f49beb91
Darron McNutt <darronlmcnutt@gmail.com>
2019-01-12 19:42:55 -0600

    Change byte[] to ArrayList<Byte> and refactor, add BFS function, update Node instance methods

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,41 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        byte[] e = { 1, 3, 4, 8, 6, 2, 7, 0, 5 };
+        byte[] g = { 1, 2, 3, 8, 0, 4, 7, 6, 5 };
+        ArrayList<Byte> easy = new ArrayList<>();
+        easy.add((byte)1);
+        easy.add((byte)3);
+        easy.add((byte)4);
+        easy.add((byte)8);
+        easy.add((byte)6);
+        easy.add((byte)2);
+        easy.add((byte)7);
+        easy.add((byte)0);
+        easy.add((byte)5);
+        ArrayList<Byte> goal = new ArrayList<>();
+        goal.add((byte)1);
+        goal.add((byte)2);
+        goal.add((byte)3);
+        goal.add((byte)8);
+        goal.add((byte)0);
+        goal.add((byte)4);
+        goal.add((byte)7);
+        goal.add((byte)6);
+        goal.add((byte)5);
 
-        byte[] board = new byte[9];
-        int zeroLocation;
+        Node testNode = new Node(easy,null,null,0,0);
 
-        // Generate sample board
-        for(byte i = 0; i < board.length; i++) {
-            board[i] = i;
+
+        ArrayList<Node> solution = breadthFirstSearch(testNode, goal);
+
+               for (Node node : solution) {
+            System.out.println(node.toString());
         }
 
-
+/*
         for (int i = 0; i < 9; i++) {
             System.out.println("Level: " + i);
             zeroLocation = getZeroLocation(board);
@@ -23,26 +46,25 @@ public class Main {
             }
             if (i+1 < 9) { board = cloneAndSwap(board,i,i+1); }
         }
+*/
 
     }
 
-    public static byte[] cloneAndSwap(byte[] board, int a, int b) {
+    public static ArrayList<Byte> cloneAndSwap(ArrayList<Byte> board, int a, int b) {
 
         // Clone the puzzle board
-        byte[] newBoard = Arrays.copyOf(board, board.length);
-        
+        ArrayList<Byte> newBoard = new ArrayList<>(board);
+
         // Swap a and b
-        byte temp = newBoard[a];
-        newBoard[a] = newBoard[b];
-        newBoard[b] = temp;
+        Collections.swap(newBoard,a,b);
 
         //Return new puzzle board
         return newBoard;
     }
 
-    public static byte[] performAction(byte[] board, int zeroLocation, Action action) {
+    public static ArrayList<Byte> performAction(ArrayList<Byte> board, int zeroLocation, Action action) {
 
-        byte[] newBoard;
+        ArrayList<Byte> newBoard;
 
         switch(action) {
             case UP:
@@ -65,20 +87,17 @@ public class Main {
         return newBoard;
     }
 
-    public static String boardToString(byte[] board) {
+    public static String boardToString(ArrayList<Byte> board) {
 
         return
-            board[0] + " " + board[1] + " " + board[2] + "\n" +
-            board[3] + " " + board[4] + " " + board[5] + "\n" +
-            board[6] + " " + board[7] + " " + board[8] + "\n";
+            board.get(0) + " " + board.get(1) + " " + board.get(2) + "\n" +
+            board.get(3) + " " + board.get(4) + " " + board.get(5) + "\n" +
+            board.get(6) + " " + board.get(7) + " " + board.get(8) + "\n";
 
     }
 
-    public static int getZeroLocation(byte[] board) {
-        for(int i = 0; i < board.length; i++) {
-            if (board[i] == 0) return i;
-        }
-        return -1;
+    public static int getZeroLocation(ArrayList<Byte> board) {
+        return board.indexOf((byte) 0);
     }
 
     public static Action[] getActions(int zeroLocation) {
@@ -117,5 +136,45 @@ public class Main {
                 actions = new Action[0];
         }
         return actions;
+    }
+
+    public static ArrayList<Node> breadthFirstSearch(Node root, ArrayList<Byte> goal) {
+
+        if (root.getState().equals(goal)) {
+            ArrayList<Node> path = new ArrayList<>();
+            path.add(root);
+            return path;
+        }
+
+        ArrayDeque<Node> searchQueue = new ArrayDeque<>();
+        searchQueue.add(root);
+
+        HashSet<ArrayList<Byte>> explored = new HashSet<>();
+
+        while(!searchQueue.isEmpty()) {
+            Node node = searchQueue.remove();
+            ArrayList<Byte> nodeState = node.getState();
+
+            explored.add(nodeState);
+            int zeroLocation = node.getZeroLocation();
+
+            node.setExpanded(true);
+
+            for(Action action : getActions(zeroLocation)) {
+                ArrayList<Byte> childState = performAction(nodeState, zeroLocation, action);
+                if (!explored.contains(childState)) {
+                    Node child = new Node(childState, node, action, node.getDepth() + 1, childState.get(zeroLocation));
+                    node.addChild(child);
+                    if (childState.equals(goal)) {
+                        return child.getPath();
+                    }
+                    searchQueue.add(child);
+                }
+            }
+
+        }
+
+        return new ArrayList<>();
+
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Node {
 
-    private byte[] state;
+    private ArrayList<Byte> state;
 
     private Node parent;
     private ArrayList<Node> children;
@@ -12,7 +13,7 @@ public class Node {
     private int cost;
     private boolean expanded;
 
-    public Node(byte[] state, Node parent, Action action, int depth, int cost) {
+    public Node(ArrayList<Byte> state, Node parent, Action action, int depth, int cost) {
         this.state = state;
         this.parent = parent;
         this.action = action;
@@ -21,5 +22,52 @@ public class Node {
 
         this.expanded = false;
         this.children = new ArrayList<>();
+    }
+
+    public ArrayList<Byte> getState() {
+        return state;
+    }
+
+    public int getZeroLocation() {
+        return this.state.indexOf((byte) 0);
+    }
+
+    public void addChild(Node child) {
+        this.children.add(child);
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setExpanded(boolean expanded) {
+        this.expanded = expanded;
+    }
+
+    public ArrayList<Node> getPath() {
+        ArrayList<Node> path = new ArrayList<>();
+        path.add(this);
+
+        Node ptr = this.parent;
+        while(ptr != null) {
+            path.add(ptr);
+            ptr = ptr.parent;
+        }
+
+        Collections.reverse(path);
+
+        return path;
+
+    }
+
+    @Override
+    public String toString() {
+
+        return
+            this.state.get(0) + " " + this.state.get(1) + " " + this.state.get(2) + "\n" +
+            this.state.get(3) + " " + this.state.get(4) + " " + this.state.get(5) + "\n" +
+            this.state.get(6) + " " + this.state.get(7) + " " + this.state.get(8) + "\n" +
+            "Zero moved: " + this.action + " Cost: " + this.cost + " Depth: " + this.depth;
+
     }
 }
